d5fe40e753ee192382937a6cd9e6c518fb4403c8
Drolpg <leandronascimento.drolpg21@gmail.com>
2024-04-03 20:38:32 -0300

    improve: addin try in events repository

--- src/models/repository/events_repository_test.py
+++ src/models/repository/events_repository_test.py
@@ -20,8 +20,8 @@ def test_insert_event():
 
 @pytest.mark.skip(reason="Não necessita")
 def test_get_event_by_id():
-    event_id = "meu-uuid-e-nois2"
+    event_id = "meu-uuid-e-nois2121"
     events_repository = EventsRepository()
     response = events_repository.get_event_by_id(event_id)
     print(response)
-    print(response.title)
+    # print(response.title)
